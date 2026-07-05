6a164e7adcfdc3fe63190ec1b5e60f84122944b7
Alexander Elias <alex.steven.elias@gmail.com>
2026-07-05 13:00:56 -0700

    start part 2

--- count_schools.py
+++ count_schools.py
@@ -1,35 +1,28 @@
-import csv
-import json
-from tools import format
-
-# read file with correct encoding
-stream = open('./school_data.csv', encoding='cp1252', newline='')
-reader = csv.reader(stream, delimiter=',')
-
-# What city has the most schools in it? How many schools does it have in it?
-# How many unique cities have at least one school in it?
+from tools import format, schools
 
 def print_counts():
+    rows = schools()
 
     headers:list[str] = None
 
+    uniqueSchools = 0
     totalSchools:int = 0
     schoolsByState = {}
     schoolsByLocale = {}
-    mostSchools = 
+    schoolsByCity = {}
 
-    for row in reader:
+    for row in rows:
 
         # capture headers and skip counting
         if not headers:
             headers = row
-            print(headers)
             continue
 
         if len(headers) != len(row):
             raise Exception('row length not valid')
 
         # row values
+        city = row[4]
         state = row[5]
         locale = row[8]
 
@@ -50,11 +43,24 @@ def print_counts():
         else:
             schoolsByLocale[locale] = 1
 
+        # collect city
+        if city in schoolsByCity:
+            schoolsByCity[city] += 1
+        else:
+            schoolsByCity[city] = 1
+            # How many unique cities have at least one school in it?
+            uniqueSchools += 1
 
         # print(row)
+
+    # What city has the most schools in it? How many schools does it have in it?
+    schoolsByCitySorted = sorted(schoolsByCity.items(), key=lambda item: item[1], reverse=True)
+    mostSchoolsByCity = schoolsByCitySorted[0]
 
     print(f'Total Schools: {totalSchools}')
     print(f'Schools by State: {format(schoolsByState)}')
     print(f'Schools by Metro-centric locale: {format(schoolsByLocale)}')
+    print(f'City with most schools: {mostSchoolsByCity[0]} ({mostSchoolsByCity[1]} Schools)')
+    print(f'Unique Schools: {uniqueSchools}')
 
 print_counts()
--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 def format(data: dict | list)-> str:
     s = json.dumps(data, indent=2, sort_keys=True)
@@ -7,3 +8,9 @@ def format(data: dict | list)-> str:
     s = s.replace('"', '')
     s = s.replace(',', '')
     return s
+
+def schools () -> list[str]:
+    # read file with correct encoding
+    stream = open('./school_data.csv', encoding='cp1252', newline='')
+    reader = csv.reader(stream, delimiter=',')
+    return list(reader)
